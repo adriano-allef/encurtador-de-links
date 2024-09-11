711fc4e7fca0c75ed734167e454ab45dde7404d6
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-10 21:40:23 -0300

    Implementado a função de obter o link

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import CadastroLink from "./controladores/CadastroLinkControlador";
+import ObterLink from "./controladores/ObterLinkControlador";
 
 const rotas = Router()
 
 rotas.post('/', new CadastroLink().controlador)
-rotas.get('/:identificador')
+rotas.get('/:identificador', new ObterLink().controlador)
 
 
 
