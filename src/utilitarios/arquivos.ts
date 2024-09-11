81d92a0ebb5ee81c6369ac808965fc929c5054e9
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-10 21:26:06 -0300

    Implementação da funçao de contar visitas no link

--- src/utilitarios/arquivos.ts
+++ src/utilitarios/arquivos.ts
@@ -14,3 +14,25 @@ export async function adicionarDados( link: Link){
     dados.push(link)
     await fs.writeFile(caminhoBancoDeDados, JSON.stringify(dados, null, '\t'))
 }
+
+export async function contarVisitas( identificador: string){
+    const dados = await lerDados()
+
+    const link = dados.find(link => {
+        return link.identificador === identificador
+    })
+
+    const linkComVIsita: Link = {
+        identificador: link!.identificador,
+        url: link!.url,
+        visitas: link!.visitas + 1
+    }
+
+    const indice = dados.findIndex(link => {
+        return link.identificador === identificador
+    })
+
+    dados.splice(indice, 1, linkComVIsita)
+
+    await fs.writeFile(caminhoBancoDeDados, JSON.stringify(dados, null, '\t'))
+}
